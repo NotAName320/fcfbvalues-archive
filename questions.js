dc874f541d3c332fa3bf931067396afe0e60e74c
Ray Chen <RC144@outlook.com>
2021-05-06 20:35:04 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,10 +1,55 @@
 questions = [
     {
-        "question": "Oppression by corporations is more of a concern than oppression by governments.",
+        "question": "I prefer a run based offense such as pistol or flexbone.",
         "effect": {
             "econ": 10,
             "dipl": 0,
             "govt": -5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I prefer a pass based offense such as air raid or spread.",
+        "effect": {
+            "econ": -10,
+            "dipl": 0,
+            "govt": 5,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I run 100% of the time; passing is out of the question.",
+        "effect": {
+            "econ": 20,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "While some may claim skill to guessing numbers, FCFB at its core is random.",
+        "effect": {
+            "econ": 0,
+            "dipl": -15,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "It is good to keep a scouting sheet which you check frequently.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": -5
+        }
+    },
+    {
+        "question": "If the scouting data is contrary to my instinct, I'll go with my instinct.",
+        "effect": {
+            "econ": 0,
+            "dipl": -10,
+            "govt": 0,
             "scty": 0
         }
     },
