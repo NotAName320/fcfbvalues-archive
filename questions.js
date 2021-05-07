1bb19ab9bef17fb3464f3708366474b5a3eb7307
Ray Chen <RC144@outlook.com>
2021-05-06 21:16:04 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -18,6 +18,24 @@ questions = [
         }
     },
     {
+        "question": "In time sensitive situations, even if I were using Flexbone, I would pass the ball.",
+        "effect": {
+            "econ": -5,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I pass even to kill the clock.",
+        "effect": {
+            "econ": -15,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
         "question": "I run 100% of the time; passing is out of the question.",
         "effect": {
             "econ": 20,
@@ -51,6 +69,78 @@ questions = [
             "dipl": -10,
             "govt": 0,
             "scty": 0
+        }
+    },
+    {
+        "question": "Scouting yourself is a good way to improve at FCFB.",
+        "effect": {
+            "econ": 0,
+            "dipl": 10,
+            "govt": 0,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I rarely punt or go for field goals on fourth down.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 15,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Onside kicking when it's not quite necessary is a good strategy.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 20,
+            "scty": 0
+        }
+    },
+    {
+        "question": "Sometimes, when you're down deep in your own territory, it's better to just cut your losses and punt.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": 0
+        }
+    },
+    {
+        "question": "I tend to play fake football like real football.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -5
+        }
+    },
+    {
+        "question": "I have called 1212 at least 15 times in the last 3 games.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 10
+        }
+    },
+    {
+        "question": "I've called 462 a lot recently.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": 0,
+            "scty": 15
+        }
+    },
+    {
+        "question": "I try to avoid meme numbers in big situations for the risk of losing it all.",
+        "effect": {
+            "econ": 0,
+            "dipl": 0,
+            "govt": -10,
+            "scty": -10
         }
     },
 ];
