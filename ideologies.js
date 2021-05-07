634ecaff31dd40d58c7dfe14240c75e88bcb2954
Ray Chen <RC144@outlook.com>
2021-05-06 21:01:23 -0700

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -17,4 +17,22 @@ ideologies = [
             "scty": 100
         }
     },
+    {
+        "name": "Duke",
+        "stats": {
+            "econ": 100,
+            "dipl": 30,
+            "govt": 70,
+            "scty": 5
+        }
+    },
+    {
+        "name": "Naki",
+        "stats": {
+            "econ": 100,
+            "dipl": 30,
+            "govt": 80,
+            "scty": 90
+        }
+    },
 ];
